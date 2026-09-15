d636f4e91b2d15c8d8836be670c0181e9e3854fe
Gorajana Mahesh <maheshmahi86089@gmail.com>
2026-09-15 20:08:33 +0530

    Create LoanInf.java
    loan abstract methods

---
+++ LoanInf.java
@@ -0,0 +1,21 @@
+package com.oops.inheritance;
+
+public interface LoanInf {
+
+	String getName();
+	
+	double getSalary();
+	
+	int getAge();
+	
+	int cibil();
+	
+	boolean isPhoneValid();
+	
+	boolean isAadharValid();
+	
+	boolean isPANValid();
+	
+	double getRIT(int cibil);
+	
+}
